c54396c13e406f9746a788ee57ea39e6ea2485c2
Dale Shlass <dale@shlass.com>
2019-11-18 17:33:34 -0500

    static page sitemap added.

--- next.config.js
+++ next.config.js
@@ -1,18 +1,22 @@
 const withSass = require("@zeit/next-sass");
-module.exports = withSass({
-  exportPathMap: function() {
-    return {
-      "/": { page: "/" }
-    };
-  }
-});
-
 const sitemap = require("nextjs-sitemap-generator");
 sitemap({
   baseUrl: "https://the-kana.com",
   pagesDirectory: __dirname + "/pages",
   targetDirectory: "static/"
 });
+module.exports = withSass({
+  exportPathMap: function() {
+    return {
+      "/": { page: "/" },
+      "/hiragana": { page: "/hiragana" },
+      "/katakana": { page: "/katakana" },
+      "/hiragana/[set]": { page: "/hiragana/[set]" },
+      "/katakana/[set]": { page: "/katakana/[set]" }
+    };
+  }
+});
+
 
 // const withSass = require('@zeit/next-sass');
 // const withCSS = require('@zeit/next-css');
