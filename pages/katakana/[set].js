aa93a8c6d5419299ff5f24f3840238a2af7a9ecf
Dale Shlass <dale@shlass.com>
2019-11-20 11:24:37 -0500

    adding global analytics.

--- pages/katakana/[set].js
+++ pages/katakana/[set].js
@@ -157,17 +157,17 @@ const Set = () => {
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Free hiragana test." />
         <title>Hiragana Test | The Kana</title>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-142294407-2"></script>
           <script dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-WBWHN5N');`,
+            __html: `window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-142294407-2');`,
           }}>
           </script>
       </Head>
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WBWHN5N"
-          style={{ display: 'none', visibility: 'hidden', height: 0, width: 0 }}></iframe></noscript>
+
       <Nav />
       <Header />
       <h1>Test</h1>
